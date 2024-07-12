916caf7d30684faaf37218512a114d97aa0bfae6
areel007 <morenikejisunday@gmail.com>
2024-07-12 01:09:07 -0700

    validate form input

--- src/middlewares/validate-form.ts
+++ src/middlewares/validate-form.ts
@@ -5,11 +5,11 @@ export const validateForm = [
   body("firstname")
     .isString()
     .isLength({ min: 3 })
-    .withMessage("firstname must be at least 3 characters long"),
+    .withMessage("Firstname must be at least 3 characters long"),
   body("lastname")
     .isString()
     .isLength({ min: 3 })
-    .withMessage("lastname must be at least 3 characters long"),
+    .withMessage("Lastname must be at least 3 characters long"),
   body("email").isEmail().withMessage("Please provide a valid email"),
   body("address").isString(),
   body("city").isString(),
@@ -17,5 +17,7 @@ export const validateForm = [
   body("state").isString(),
   body("zipCode").isString(),
   body("ssn").isString(),
-  body("mobile").isMobilePhone("en-US"),
+  body("mobile")
+    .isMobilePhone("en-US")
+    .withMessage("Please provide a valid mobile number"),
 ];
